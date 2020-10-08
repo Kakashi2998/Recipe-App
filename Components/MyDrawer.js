d7529d35f09904c35cf31c4f9f0213108a878022
Apoorv Mathur <apoorvmathur.29@gmail.com>
2020-10-08 19:45:24 +0530

    search bar focussed on focus and fixed drawer accordion issue

--- Components/MyDrawer.js
+++ Components/MyDrawer.js
@@ -1,8 +1,9 @@
 import { DrawerContentScrollView } from '@react-navigation/drawer';
-import { Accordion, Button, List, ListItem, Switch, Text, View } from 'native-base';
+import { Accordion, Button, ListItem, Switch, Text, View } from 'native-base';
 import React from 'react';
 import { Image } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { Divider, List } from 'react-native-paper';
 import { useDispatch, useSelector } from 'react-redux';
 import CategoryData from '../Data/dummyCategories';
 import Categories from '../Screens/Categories';
@@ -12,7 +13,6 @@ const MyDrawer = ({navigation}) => {
 
     const categories = useSelector(state => state.CategoryReducer.categories);
 
-    // const veg = useSelector(state => state.RecipeReducer.veg);
     const [veg, setVeg] = React.useState(false);
 
     const changeVeg = () => {
@@ -26,36 +26,20 @@ const MyDrawer = ({navigation}) => {
         <DrawerContentScrollView>
             <Image source={{uri: 'https://image.winudf.com/v2/image/Y29tLnRhc3R5Lm15LnJlY2lwZS5ib29rLmNvb2tib29rLmFwcF9pY29uXzE1MjIyNzQ0ODdfMDUz/icon.png?w=170&fakeurl=1'}}
             style={{width: 150, height: 150, alignSelf: 'center'}}/>
+            <Divider/>
             <View style={{flexDirection: 'row', paddingHorizontal: 30, paddingVertical: 10}}>
                         <Text style={{fontSize: 20}}>Veg only</Text>
                         <Switch value={veg} onValueChange={changeVeg}/>
             </View>
-            <List>
-                <ListItem>
-                    <Button onPress={() => navigation.navigate('Categories')} transparent>
-                        <Text>All Recipes</Text>
-                    </Button>
-                </ListItem>
-                <ListItem>
-                    <Button onPress={() => navigation.navigate('Favourites')} transparent>
-                        <Text>Favourites</Text>
-                    </Button>
-                </ListItem>
-                <ListItem>
-                    <Accordion dataArray={[{title: 'Categories', content: 
-                        <List>
-                            {categories.map(cat => 
-                                <ListItem key={cat.id + '1'}> 
-                                    <Button transparent 
-                                    onPress={() => navigation.navigate('Category Recipes', {...cat})}>
-                                        <Text>{cat.title}</Text>
-                                    </Button>
-                                </ListItem>
-                            )}
-                        </List>
-                    }]}/>
-                </ListItem>
-            </List>
+            <Divider/>
+            <List.Item onPress={() => navigation.navigate('Categories')} title='All Recipes'/>
+            <List.Item onPress={() => navigation.navigate('Favourites')} title='Favourites'/>
+            <List.Accordion title='Categories'>
+                {categories.map(cat => 
+                    <List.Item title={cat.title} key={cat.id + '1'}
+                    onPress={() => navigation.navigate('Category Recipes', {...cat})}/>
+                )}
+            </List.Accordion>
         </DrawerContentScrollView>
     );
 }
--- Screens/SearhRecipes.js
+++ Screens/SearhRecipes.js
@@ -1,6 +1,7 @@
-import { Body, Container, Content, Header, Icon, Input, Item, Title, View } from 'native-base';
+import { useFocusEffect, useIsFocused } from '@react-navigation/native';
+import { Body, Button, Container, Content, Header, Icon, Input, Item, Text, Title, View } from 'native-base';
 import React, { useRef } from 'react';
-import { FlatList, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { FlatList, Keyboard, TextInput, TouchableWithoutFeedback } from 'react-native';
 import { useSelector } from 'react-redux';
 import RecipeCard from '../Components/RecipeCard';
 
@@ -13,6 +14,9 @@ const SearchRecipes = props => {
 
     const [text, setText] = React.useState('');
 
+    const searchBar = React.useRef();
+    const isFocused = useIsFocused();
+
     const search = (input) => {
         setText(input);
         if(input === ''){
@@ -24,6 +28,11 @@ const SearchRecipes = props => {
             (isVeg? r.vegetarian: true)));
     }
 
+    React.useEffect(() => {
+            if(isFocused)
+                searchBar.current.focus();
+    }, [isFocused])
+
     return (
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
             <Container>
@@ -31,13 +40,17 @@ const SearchRecipes = props => {
                 <Header searchBar rounded>
                     <Item rounded>
                         <Icon name='ios-search'/>
-                        <Input placeholder='Search' onChangeText={search} value={text}/>
+                        <TextInput placeholder='Search' onChangeText={search} value={text} ref={searchBar}
+                        style={{width: '80%'}}/>
                         {text === '' ? null : 
                         <Icon name='md-close' onPress={() => {setText(''); setRecipes([]);}}/>
                         }
                     </Item>
                 </Header>
                 <View style={{paddingHorizontal: 10}}>
+                    {/* <Button onPress={() => searchBar.current.focus()}>
+                        <Text>Focus</Text>
+                    </Button> */}
                     <FlatList data={recipes} renderItem={data => <RecipeCard item={data.item}/>}/>
                 </View>
             </Container>
